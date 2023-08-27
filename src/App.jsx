f0f860c553c0d9bc789b8888183025fc15b60729
EGNKupava <kupavaegn@gmail.com>
2023-08-27 12:27:17 +0300

    feat: init

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import { Button } from '@alfalab/core-components/button/esm';
+
+function App() {
+  return (
+    <div className='app'>
+      <Button view='accent'>
+         App
+      </Button>
+    </div>
+  )
+}
+
+export default App
