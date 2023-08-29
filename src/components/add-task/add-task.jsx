6a2e27788477cb0a2586eaeb4ca33e6172eb8fef
EGNKupava <kupavaegn@gmail.com>
2023-08-29 15:07:48 +0300

    feat: fix move

--- src/components/add-task/add-task.jsx
+++ src/components/add-task/add-task.jsx
@@ -1,48 +1,60 @@
-import { useState } from 'react';
-import { AddLargeMIcon } from '@alfalab/icons-glyph/AddLargeMIcon';
-import { Input } from '@alfalab/core-components/input/esm';
-import { IconButton } from '@alfalab/core-components/icon-button/esm';
-import { Gap } from '@alfalab/core-components/gap/esm';
-import { Textarea } from '@alfalab/core-components/textarea/esm';
+import {useState} from "react";
+import {AddLargeMIcon} from "@alfalab/icons-glyph/AddLargeMIcon";
+import {Input} from "@alfalab/core-components/input/esm";
+import {IconButton} from "@alfalab/core-components/icon-button/esm";
+import {Gap} from "@alfalab/core-components/gap/esm";
+import {Textarea} from "@alfalab/core-components/textarea/esm";
 
 import "./add-task.css";
-import { useTodoStore } from '../../store/todo';
+import {useTodoStore} from "../../store/todo";
 
 export const AddTask = () => {
-  const [inputValue, setInputValue] = useState('');
-  const [textareaValue, setTextareaValue] = useState('');
+  const [inputValue, setInputValue] = useState("");
+  const [textareaValue, setTextareaValue] = useState("");
 
   const addTask = useTodoStore((state) => state.addTask);
 
   const handleAddTaskClick = () => {
     if (inputValue) {
-      addTask({id: +Date.now(), title: inputValue, description: textareaValue, isDone: false })
-      setInputValue('');
-      setTextareaValue('');
+      addTask({
+        id: String(Date.now()),
+        title: inputValue,
+        description: textareaValue,
+        isDone: false,
+      });
+      setInputValue("");
+      setTextareaValue("");
     }
-  }
+  };
   const handleInputChange = (_, {value}) => {
     setInputValue(value);
-  }
+  };
   const handleTextareaChange = (_, {value}) => {
     setTextareaValue(value);
-  }
+  };
 
   return (
-      <div className='add-task'>
-        <div className='top-block'>
-          <Input block={true} label='Название задачи' size='m' value={inputValue} onChange={handleInputChange}/>
-          <Gap direction="horizontal" size='m'/>
-          <IconButton icon={AddLargeMIcon} onClick={handleAddTaskClick}/>
-        </div>
-        <Textarea
-                label='Описание'
-                minRows={2}
-                autosize={false}
-                block={true}
-                labelView="inner"
-                value={textareaValue}
-                onChange={handleTextareaChange}
-            />
+    <div className="add-task">
+      <div className="top-block">
+        <Input
+          block={true}
+          label="Название задачи"
+          size="m"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+        <Gap direction="horizontal" size="m" />
+        <IconButton icon={AddLargeMIcon} onClick={handleAddTaskClick} />
       </div>
-)};
+      <Textarea
+        label="Описание"
+        minRows={2}
+        autosize={false}
+        block={true}
+        labelView="inner"
+        value={textareaValue}
+        onChange={handleTextareaChange}
+      />
+    </div>
+  );
+};
--- src/store/todo.js
+++ src/store/todo.js
@@ -1,4 +1,5 @@
 import {create} from "zustand";
+import {devtools} from "zustand/middleware";
 
 const initialTodos = {
   1: {id: 1, title: "json-server", description: "todo smt", isDone: false},
@@ -9,52 +10,54 @@ const initialOrder = ["1"];
 const delay = async (time = 1000) =>
   new Promise((res) => setTimeout(res, time));
 
-export const useTodoStore = create((set) => ({
-  todos: initialTodos,
-  order: initialOrder,
-  isLoading: false,
-  fetch: async () => {
-    set({isLoading: true});
-    await delay(3000);
-    try {
-      const res = await fetch("http://localhost:3000/todo");
-      const {todos, order} = await res.json();
-      set({todos, order, isLoading: false});
-    } catch (err) {
-      console.error("Error", err);
-    }
-  },
-  addTask: (task) =>
-    set((state) => ({
-      todos: {
-        ...state.todos,
-        [task.id]: task,
-      },
-      order: [task.id, ...state.order],
-    })),
-  removeTask: (currentId) =>
-    set((state) => {
-      const itemPosition = state.order.findIndex((id) => id === currentId);
-      const todosCopy = {...state.todos};
-      const copyOrder = [...state.order];
-      copyOrder.splice(itemPosition, 1), delete todosCopy[currentId];
-      return {
-        order: copyOrder,
-        todos: todosCopy,
-      };
-    }),
-  changeStatus: (currentId, isDone) =>
-    set((state) => ({
-      todos: {
-        ...state.todos,
-        [currentId]: {
-          ...state.todos[currentId],
-          isDone,
+export const useTodoStore = create(
+  devtools((set) => ({
+    todos: initialTodos,
+    order: initialOrder,
+    isLoading: false,
+    fetch: async () => {
+      set({isLoading: true});
+      await delay(3000);
+      try {
+        const res = await fetch("http://localhost:3000/todo");
+        const {todos, order} = await res.json();
+        set({todos, order, isLoading: false});
+      } catch (err) {
+        console.error("Error", err);
+      }
+    },
+    addTask: (task) =>
+      set((state) => ({
+        todos: {
+          ...state.todos,
+          [task.id]: task,
         },
-      },
-    })),
-  move: (order) =>
-    set({
-      order,
-    }),
-}));
+        order: [task.id, ...state.order],
+      })),
+    removeTask: (currentId) =>
+      set((state) => {
+        const itemPosition = state.order.findIndex((id) => id === currentId);
+        const todosCopy = {...state.todos};
+        const copyOrder = [...state.order];
+        copyOrder.splice(itemPosition, 1), delete todosCopy[currentId];
+        return {
+          order: copyOrder,
+          todos: todosCopy,
+        };
+      }),
+    changeStatus: (currentId, isDone) =>
+      set((state) => ({
+        todos: {
+          ...state.todos,
+          [currentId]: {
+            ...state.todos[currentId],
+            isDone,
+          },
+        },
+      })),
+    move: (order) =>
+      set({
+        order,
+      }),
+  }))
+);
