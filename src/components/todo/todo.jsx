66e096a13e022cdfb5a8bc1dc2d3078e5e1b4b56
EGNKupava <kupavaegn@gmail.com>
2023-08-29 13:14:31 +0300

    feat: fetch

--- src/components/todo/todo.jsx
+++ src/components/todo/todo.jsx
@@ -1,23 +1,35 @@
 import { Divider } from '@alfalab/core-components/divider/esm';
 import { Gap } from '@alfalab/core-components/gap/esm';
+import { Spinner } from '@alfalab/core-components/spinner/esm';
 
 import { AddTask } from '../add-task/add-task';
 import { Task } from '../task/task';
 import { useTodoStore } from '../../store/todo';
 
 import './todo.css';
+import { useEffect } from 'react';
 
 export const Todo = () => {
-  const order = useTodoStore((state) => state.order);
-  const todos = useTodoStore((state) => state.todos);
+  const {order, todos, fetch, isLoading} = useTodoStore((state) => ({
+    order: state.order, 
+    todos: state.todos,
+    fetch: state.fetch,
+    isLoading: state.isLoading,
+  }));
+
+  useEffect(() => {
+    fetch();
+  }, [])
 
   return (
     <div className='todo'>
+      
       <AddTask />
       <Divider />
       <Gap size="m"/>
+      <Spinner visible={isLoading} />
       <div className='task-list'>
-      {
+      { !isLoading &&
         order.map((taskId) => {
           const { title, description, isDone} = todos[taskId];
           return <Task key={taskId} id={taskId} title={title} description={description} isDone={isDone}/>
--- src/store/todo.js
+++ src/store/todo.js
@@ -6,9 +6,23 @@ const initialTodos = {
 
 const initialOrder = ["1"];
 
+const delay = async (time = 1000) => new Promise((res) => setTimeout(res, time));
+
 export const useTodoStore = create((set) => ({
   todos: initialTodos,
   order: initialOrder,
+  isLoading: false,
+  fetch: async () => {
+    set({isLoading: true});
+    await delay(3000);
+    try {
+      const res = await fetch('http://localhost:3000/todo');
+      const { todos, order} = await res.json();
+      set({ todos, order, isLoading: false });
+    } catch(err) {
+      console.error('Error', err);
+    }
+  },
   addTask: (task) => set((state) => ({ 
     todos: {
       ...state.todos,
